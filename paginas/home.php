52ae321ed57fe5fac658965ffec7e78e75280e29
KalfelsWiggers <gabriel_wiggers@estudante.sesisenai.org.br>
2023-08-23 08:31:13 -0300

    a

---
+++ paginas/home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atendimento a ocorrências</title>
+    <script src="../scripts/jquery-1.8.2.min.js"></script>
+    <link rel="stylesheet" href="../styles/style.css">
+</head>
+
+<?php
+    session_start();
+?>
+
+<body>
+    <header>
+        <img src="../sources/logo_pequena.png">
+        <ul>
+            <li><a href="#">Ocorrência</a></li>
+            <li><a href="#">Suporte</a></li>
+            <li><a href="#">Fazer login</a></li>
+        </ul>
+    </header>
+    <main>
+        <section>
+            <h2>Bem vindo a plataforma de ocorrências!</h2>
+            <button>Clique aqui para login</button>
+        </section>
+    </main>
+</body>
+</html>
