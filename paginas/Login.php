4d3bf6f437ba42af597dd7fc2316a7060ebdc7e7
KalfelsWiggers <gabriel_wiggers@estudante.sesisenai.org.br>
2023-08-31 10:27:38 -0300

    a

--- paginas/Login.php
+++ paginas/Login.php
@@ -1,29 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Ocorrência</title>
+    <title>Atendimento a ocorrências</title>
     <script src="../scripts/jquery-1.8.2.min.js"></script>
-    <link rel="stylesheet" href="../styles/Ocorrencia.css">
+    <link rel="stylesheet" href="../styles/Login.css">
 </head>
+
 <body>
     <header>
         <img src="../sources/logo_pequena.png">
         <ul>
-            <li><a href="Home.php">Retornar</a></li>
+            <li><a href="Ocorrencia.php">Ocorrência</a></li>
             <li><a href="#">Suporte</a></li>
-            <li><a href="#">Fazer login</a></li>
+            <li><a href="Home.php">Retornar</a></li>
         </ul>
     </header>
     <main>
         <section>
-            <div class="txt">
-                <h2>Folha de Atendimento</h2>
-                <h3>Preencha todos os campos adequadamente</h3>
-            </div>
-
+            
         </section>
     </main>
 </body>
--- paginas/home.php
+++ paginas/home.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atendimento a ocorrências</title>
     <script src="../scripts/jquery-1.8.2.min.js"></script>
-    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/Home.css">
 </head>
 
 <?php
@@ -16,12 +16,23 @@
 <body>
     <header>
         <img src="../sources/logo_pequena.png">
-        <ul>
+        <ul class="ul-1">
             <li><a href="Ocorrencia.php">Ocorrência</a></li>
             <li><a href="#">Suporte</a></li>
-            <li><a href="#">Fazer login</a></li>
+            <li><a href="Login.php">Fazer login</a></li>
         </ul>
+        <nav class="hidden_nav">
+            <ul class="ul-2">
+                <li><a href="Ocorrencia.php">Ocorrência</a></li>
+                <li><a href="#">Suporte</a></li>
+                <li><a href="Login.php">Fazer login</a></li>
+            </ul>
+        </nav>
+        <div class="menu">
+            <div class="menu_burguer"></div>
+        </div>
     </header>
+
     <main>
         <section>
             <h2>Bem vindo a plataforma de ocorrências!</h2>
@@ -29,4 +40,5 @@
         </section>
     </main>
 </body>
+<script src="../scripts/Home.js"></script>
 </html>
